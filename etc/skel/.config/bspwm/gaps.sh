7acaaa0119d4067bfb07848d75849229daa2e89d
creio <mailcreio@gmail.com>
2026-04-20 15:28:40 +0300

    20.04.2026 Update

--- etc/skel/.config/bspwm/gaps.sh
+++ etc/skel/.config/bspwm/gaps.sh
@@ -5,10 +5,10 @@ status=$(bspc config -d focused window_gap)
 if [ $status = 0 ]; then
   bspc config border_width         2
   bspc config -d focused window_gap      10
-  bspc config -d focused top_padding     25
-  bspc config -d focused bottom_padding  25
-  bspc config -d focused left_padding   50
-  bspc config -d focused right_padding  50
+  bspc config -d focused top_padding     15
+  bspc config -d focused bottom_padding  15
+  bspc config -d focused left_padding   25
+  bspc config -d focused right_padding  25
 else
   bspc config border_width         2
   bspc config -d focused window_gap       0
